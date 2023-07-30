b829bfa6396d03d3df0a20193b85b6d7934482e6
egongolara <134380417+egongolara@users.noreply.github.com>
2023-07-30 23:53:12 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x567b1d6d9c5bd93f38e91f9621d5af4e81ff0444";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
